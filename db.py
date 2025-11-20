d1b19b41cac51fd7d1a3b19051851ae2d0a8076b
Daria Konovalova <159564599+doshikd@users.noreply.github.com>
2025-11-21 01:00:37 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,17 +1,31 @@
 # db.py
-import pymysql
 import os
+import pymysql
+from PyQt6.QtWidgets import QMessageBox, QApplication
+
+
+
+
 
 
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+
+
+
+
 def get_connection():
-    return pymysql.connect(
-        host='127.127.126.6',
-        user='root',
-        password='',  # ← подставьте свой пароль
-        database='repair_db',
-        charset='utf8mb4',
-        autocommit=True
-    )
+    try:
+        return pymysql.connect(
+            host='127.0.0.1',      # ← исправлено на стандартный localhost
+            user='root',
+            password='',           # ← укажите свой пароль, если есть
+            database='repair_db',
+            charset='utf8mb4',
+            autocommit=True
+        )
+    except Exception:
+        QMessageBox.critical(None, "Ошибка БД", 
+            "Не удалось подключиться к базе данных.")
+        exit()  # закрываем программу
